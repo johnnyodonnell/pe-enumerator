034da8c11bf0808c71979c051761813cf21203e9
Johnny O'Donnell <odjohnny@amazon.com>
2022-07-05 19:23:24 -0700

    Expand scope of `find` queries

--- linux.sh
+++ linux.sh
@@ -2,17 +2,17 @@
 
 # From PWK pdf (p. 531)
 echo "----- Find all writable files  -----"
-find /usr -writable -type d 2>/dev/null
+find / -writable -not -path "/proc/*" -not -path "/sys/fs/cgroup/*" -not -path "/dev/*" 2>/dev/null
 echo ""
 
 # From PWK pdf (p. 536)
 echo "----- Find all files with setuid flag set -----"
-find /usr -perm -u=s -type f 2>/dev/null
+find / -perm -u=s -type f 2>/dev/null
 echo ""
 
 # Based on setuid search
 echo "----- Find all files with setgid flag set -----"
-find /usr -perm -g=s -type f 2>/dev/null
+find / -perm -g=s -type f 2>/dev/null
 echo ""
 
 # From https://github.com/johnnyodonnell/notes-on-oscp-lab-machines/blob/master/10.11.1.217/notes.txt
